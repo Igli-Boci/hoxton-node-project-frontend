c5ef948c70f1470caae4b56af00d5b852585e1ee
Aid Maliqi <aid.maliqi04@gmail.com>
2022-09-27 15:29:27 +0200

    added state

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -3,8 +3,61 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import SignIn from "../pages/SignIn";
 import LogIn from "../pages/LogIn";
+import { useEffect, useState } from "react";
 
 const Routers = () => {
+  const [currentUser , setCurrentUser] = useState(null)
+  const [currentUserOffers , setCurrentUserOffers ] = useState(null)
+  const [allOffers, setAllOffers] = useState(null)
+
+  function signIn (data) {
+    setCurrentUser(data.user)
+    localStorage.token = data.token
+  }
+
+  function signOut () {
+    setCurrentUser(null)
+    localStorage.removeItem("token")
+  }
+
+  useEffect(() => {
+    if (localStorage.token) {
+      fetch('http://localhost:3001/validate' , {
+        headers : {Authorization : localStorage.token}
+      })
+      .then(resp => resp.json())
+      .then(data => {
+        if (data.error) {
+          alert(data.error)
+        } else {
+          signIn(data)
+        }
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    if(localStorage.token) {
+      fetch('http://localhost:3001/offers' , {
+        headers : {Authorization : localStorage.token}
+      })
+      .then(resp => resp.json())
+      .then(data => {
+        if (data.error) {
+          alert(data.error)
+        } else {
+          setCurrentUserOffers(data)
+        }
+      })
+    }
+  } , [])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/all-offers')
+    .then(resp => resp.json())
+    .then(data => setAllOffers(data))
+  } ,[])
+
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/home" />} />
