6a4e61fa31ddbe1dd61cecb20a937b788ff8486b
Igli-Boci <igliboci10@gmai.com>
2022-10-03 10:47:55 +0200

    Adding header and footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,106 @@
+import React from "react";
+import "./footer.css";
+
+const quickLinks01 = [
+  {
+    path: "#",
+    display: "Mobile",
+  },
+  {
+    path: "#",
+    display: "IPTV",
+  },
+  {
+    path: "#",
+    display: "Internet",
+  },
+];
+
+const quickLinks02 = [
+  {
+    path: "#",
+    display: "Pricing",
+  },
+  {
+    path: "#",
+    display: "Documentation",
+  },
+  {
+    path: "#",
+    display: "Guides",
+  },
+];
+
+const quickLinks03 = [
+  {
+    path: "#about",
+    display: "About",
+  },
+  {
+    path: "#",
+    display: "Jobs",
+  },
+  {
+    path: "#blog",
+    display: "Blog",
+  },
+];
+
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__wrapper">
+          <div className="footer__logo">
+            <h2>Only the best that anyone has to offer!</h2>
+            <p className="description">Grow with us</p>
+
+            <p className="small__text description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptates praesentium soluta libero earum mollitia! Magnam hic
+              corporis cupiditate voluptas ducimus!
+            </p>
+          </div>
+
+          <div className="footer__quick-links">
+            <h3 className="quick__links-title">Offers</h3>
+            <ul className="quick__links">
+              {quickLinks01.map((item, index) => (
+                <li className="quick__link-item" key={index}>
+                  <a href={item.path}>{item.display}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="footer__quick-links">
+            <h3 className="quick__links-title">Support</h3>
+            <ul className="quick__links">
+              {quickLinks02.map((item, index) => (
+                <li className="quick__link-item" key={index}>
+                  <a href={item.path}>{item.display}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="footer__quick-links">
+            <h3 className="quick__links-title">Company</h3>
+            <ul className="quick__links">
+              {quickLinks03.map((item, index) => (
+                <li className="quick__link-item" key={index}>
+                  <a href={item.path}>{item.display}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <p className="copyright">
+          Copyright {year}, developed by Igli Boçi, Aid Maliqi. All rights
+          reserved.
+        </p>
+      </div>
+    </footer>
   );
 };
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,57 @@
+import React, { useRef, useEffect } from "react";
+import "./header.css";
+
+const nav__links = [
+  {
+    display: "Home",
+    path: "/home",
+  },
+  {
+    display: "Offers",
+    path: "/all-offers",
+  },
+  {
+    display: "Profile",
+    path: "/profile",
+  },
+  {
+    display: "LogIn",
+    path: "/logIn",
+  },
+  {
+    display: "Sign Up",
+    path: "/signUp",
+  },
+];
+
 const Header = () => {
+  useEffect(() => {
+    window.addEventListener;
+  }, []);
+
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header className="header">
+      <div className="container">
+        <div className="nav__wrapper">
+          <div className="logo">
+            <h2>LOTUS</h2>
+          </div>
+
+          {/* ========= navigation ========= */}
+          <div className="navigation">
+            <ul className="menu">
+              {nav__links.map((item, index) => (
+                <li className="menu__item">
+                  <a href={item.path} className="menu__link">
+                    {item.display}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
 
